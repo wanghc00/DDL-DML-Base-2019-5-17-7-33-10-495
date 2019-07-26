787d1e97a3b71d3967cd217a816ddbd11d0704fb
wanghc00 <306093226@qq.com>
2019-07-26 09:18:13 +0800

    作业提交

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,47 @@
 -- **数据库级别：**  
 --  显示所有数据库  
+show databases;
 
 --  进入某个数据库  
+use 数据库名;
 
 --  创建一个数据库  
+create databases 数据库名;
 
 --  创建指定字符集的数据库  
+GBK: create database 数据库名 DEFAULT CHARACTER SET gbk COLLATE gbk_chinese_ci;
+UTF8: CREATE DATABASE 数据库名 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 --  显示数据库的创建信息   
+show create database 数据库名;
 
---  修改数据库的编码  
+--  修改数据库的编码 
+alter database mydb character set 编码格式; 
 
 --  删除一个数据库   
+drop databasae 数据库名;
 
 -- **表级别**
 --  修改表名
+rename table 老表名 to  新表名;
 
 --  修改字段的数据类型
+alter table 表名 modify column 列名 新的列的类型
 
 --  修改字段名
+ALTER  TABLE 表名 CHANGE 旧字段名 新字段名 新数据类型;
 
 --  添加字段
+ALTER TABLE 表名 add 字段名 数据类型;
 
 --  删除字段
+ALTER TABLE 表名 DROP 字段名称;
 
 --  修改表的存储引擎
+alter table 表名 engine = 引擎名;
 
 --  删除表的外键约束
+ALTER TABLE 表名称 DROP FOREIGN KEY 外键约束名
 
 --  删除一张表
+DROP TABLE 表名;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
 -- 插入记录  
+INSERT INTO student VALUES ('003','王五','19','男');
 
 -- 修改记录  
+update student set student.name = '赵六',student.age='20' where student.id='003';
 
 --  删除记录  
+delete from student where student.id='003';
 
 -- 查询记录  
+select * from student;
